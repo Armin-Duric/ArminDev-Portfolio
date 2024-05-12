7008fbaa51381cd73ca9bb32014e716629217cd8
Armin <muttenroshi11@hotmail.com>
2024-05-12 21:35:56 +0200

    Initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar: React.FC = () => {
+  return (
+    <>
+      <div className="w-full bg-primary h-[20px] border-2">
+        <h1 className="text-3xl">Hello</h1>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
